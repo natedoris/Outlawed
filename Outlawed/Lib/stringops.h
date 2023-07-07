1ec0e4a95b2b34dc3c4474b0fc5af99f4519d171
nate.doris <nate.doris@gmail.com>
2023-07-06 20:45:34 -0600

    renamed enemy.h, made massive changes to some functions in battletext.cpp. Added stringops.h/cpp

---
+++ Outlawed/Lib/stringops.h
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace stringops 
+{
+	extern int slicestr(const char* src, char* dst, int start_point, int end_point);
+	int revstrcmp(const char* string1, const char* string2);
+}
